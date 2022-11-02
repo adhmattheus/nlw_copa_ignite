557cc618f880a34403ec704d82d151674cb5787b
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-11-02 13:49:09 -0300

    personalizando o statusBar e finalizando a aula 1

--- mobile/nlwcopamobile/App.tsx
+++ mobile/nlwcopamobile/App.tsx
@@ -1,8 +1,8 @@
-import { NativeBaseProvider, Text, Center } from "native-base";
 import { THEME } from './src/styles/theme'
-import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { Loading } from './src/components/Loading';
 import { SignIn } from './src/screens/Signin';
+import { NativeBaseProvider, StatusBar } from "native-base";
+import { Loading } from './src/components/Loading';
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 
 export default function App() {
@@ -11,9 +11,13 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
-      {
-        fontsLoaded ? <SignIn /> : <Loading />
-      }
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider >
   );
 }
