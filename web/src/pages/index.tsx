9e5c9bdcd510c042419abd8dbadce417a9ee00af
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-11-08 16:55:28 -0300

    versao web finalizada

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,10 +1,11 @@
 
+import { FormEvent, useState } from 'react'
+import { api } from '../lib/axios'
 import Image from 'next/image'
 import appPreviewImg from '../assets/app-nlw.png'
-import logoImg from '../assets/logo.svg'
 import usersAvatar from '../assets/avatares.png'
 import iconCheckImg from '../assets/icon.svg'
-import { api } from '../lib/axios'
+import logoImg from '../assets/logo.svg'
 
 interface HomeProps {
   poolCount: number;
@@ -13,6 +14,32 @@ interface HomeProps {
 }
 
 export default function Home(props: HomeProps) {
+
+  const [poolTitle, setPoolTitle] = useState('')
+
+  async function createPool(event: FormEvent) {
+    event.preventDefault();
+
+    try {
+      const response = await api.post('/pools', {
+        title: poolTitle,
+      });
+
+      const { code } = response.data;
+
+      await navigator.clipboard.writeText(code)
+
+      alert('Bolão criado com sucesso, código copiado para área de transferência!')
+      setPoolTitle('')
+    } catch (err) {
+      console.log(err)
+      alert('falha ao criar um bolão, tente novamente!')
+    }
+  }
+
+
+
+
 
   return (
     <div className='max-w-[1024px] h-screen mx-auto grid grid-cols-2 gap-28 items-center'>
@@ -30,11 +57,13 @@ export default function Home(props: HomeProps) {
           </strong>
         </div>
 
-        <form className='mt-8 flex gap-2'>
-          <input className='flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm'
+        <form onSubmit={createPool} className='mt-8 flex gap-2'>
+          <input className='flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm text-gray-100'
             type="text"
             required
             placeholder='Qual nome do seu bolão?'
+            onChange={event => setPoolTitle(event.target.value)}
+            value={poolTitle}
           />
           <button className='bg-yellow-500 px-6 py-4 rounded text-gray-900 font-bold text-sm uppercase hover:bg-yellow-700'
             type='submit'
